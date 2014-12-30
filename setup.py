07e4466ee9073ca4719318bf74f7f10c1e8fd59f
Alexei Romanoff <drednout.by@gmail.com>
2014-12-30 22:45:24 +0300

    Added distutils script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='xo_engine',
+    version='1.0',
+    author='Alexei Romanov',
+    author_email='drednout.by@gmail.com',
+    py_modules=['xo_engine.game'],
+    #install_requires = ['docutils>=0.3'],
+)
