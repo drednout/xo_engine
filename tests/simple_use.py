b29f2be5e4b7478b647bd051444114d3a5a29f92
Alexei Romanoff <drednout.by@gmail.com>
2014-12-30 22:32:23 +0300

    Added simple console program for testing game engine

---
+++ tests/simple_use.py
@@ -0,0 +1,45 @@
+import random
+
+import xo_engine.game as xo_engine
+
+
+game_field = xo_engine.GameField()
+player1 = xo_engine.Player("Vasya", xo_engine.CELL_STATE_X)
+player2 = xo_engine.Player("Lesha", xo_engine.CELL_STATE_O)
+game = xo_engine.Game(game_field, [player1, player2])
+
+possible_moves = []
+for i in xrange(xo_engine.DEFAULT_FIELD_WIDTH):
+    for j in xrange(xo_engine.DEFAULT_FIELD_HEIGHT):
+        move = (i, j)
+        possible_moves.append(move)
+
+move_counter = 0
+while not game.check_game_over():
+    if len(possible_moves) == 0:
+        break
+    random_move_index = random.randint(0, len(possible_moves) - 1)
+    print("DEBUG: random_move_index is {}".format(random_move_index))
+    random_move = possible_moves.pop(random_move_index)
+    x, y = random_move
+    if move_counter % 2 == 0:
+        game.make_move(player1, x, y)
+    else:
+        game.make_move(player2, x, y)
+
+    move_counter += 1
+    print(str(game_field))
+    print(repr(game_field))
+
+winner = game.get_winner()
+if winner == player1:
+    print("Player 1 wins! Winner name is {}".format(player1.name))
+elif winner == player2:
+    print("Player 2 wins! Winner name is {}".format(player2.name))
+elif winner is None:
+    print("Draw is detected! {} and {} are both loosers:)".format(player1.name,
+          player2.name))
+else:
+    raise Exception("Unexpected get_winner() obtained: {}".format(winner))
+
+
