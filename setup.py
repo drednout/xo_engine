9f864f7997fd119b09f597abf3d4968518092df7
Alexei Romanoff <drednout.by@gmail.com>
2015-01-06 09:25:09 +0300

    Several improvements were made
    1) Now setup.py uses setup function from setuptools
    2) Add more info about xo_engine python package

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='xo_engine',
     version='1.0',
     author='Alexei Romanov',
     author_email='drednout.by@gmail.com',
-    py_modules=['xo_engine.game'],
+    description = ("Simple game engine for XO game"),
+    license="GPLv2",
+    keywords="game engine xo",
+    url="https://github.com/drednout/xo_engine",
+    packages=['xo_engine'],
+    long_description=open('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Games/Entertainment :: Board Games",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
     #install_requires = ['docutils>=0.3'],
 )
