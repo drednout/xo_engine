6c5fa770f60faa86c75c4f128f245607182df966
Aleksey Romanov <a_romanov@wargaming.net>
2015-01-18 13:57:02 +0300

    Simple usage test has been actualized

--- tests/simple_use.py
+++ tests/simple_use.py
@@ -4,9 +4,11 @@ import xo_engine.game as xo_engine
 
 
 game_field = xo_engine.GameField()
-player1 = xo_engine.Player("Vasya", xo_engine.CELL_STATE_X)
-player2 = xo_engine.Player("Lesha", xo_engine.CELL_STATE_O)
+player1 = xo_engine.Player(1, "Vasya", xo_engine.CELL_STATE_X)
+player2 = xo_engine.Player(2, "Lesha", xo_engine.CELL_STATE_O)
 game = xo_engine.Game(game_field, [player1, player2])
+print(player1.as_dict())
+print(player2.as_dict())
 
 possible_moves = []
 for i in xrange(xo_engine.DEFAULT_FIELD_WIDTH):
@@ -30,8 +32,11 @@ while not game.check_game_over():
     move_counter += 1
     print(str(game_field))
     print(repr(game_field))
+    print(game_field.as_dict())
 
 winner = game.get_winner()
+print(game.as_dict())
+print(game.check_game_over())
 if winner == player1:
     print("Player 1 wins! Winner name is {}".format(player1.name))
 elif winner == player2:
